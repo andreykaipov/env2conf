60ebb14e017aad36042a8bd20b29c4f0c4263525
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2019-06-09 23:03:41 -0400

    add main parsing thing

---
+++ env2star.go
@@ -0,0 +1,169 @@
+
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+	"strconv"
+)
+
+var bitSize = 32 + int(^uintptr(0)>>63<<5)
+
+// cleans up an env var value for use in our parsed maps and arrays
+func sanitize(s string) interface{} {
+	if z, err := strconv.ParseFloat(s, bitSize); err == nil {
+		return z
+	} else if s == "{}" {
+		return map[string]interface{}{}
+	} else if s == "[]" {
+		return &VariableArray{}
+	} else {
+		if len(s) > 1 && s[0] == '"' && s[len(s)-1] == '"' {
+			s = s[1:len(s)-1]
+		}
+		return s
+	}
+}
+
+func main() {
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
+	prefixFilter := []string{"a", "b", "c", "d", "["}
+	parsed := map[string]interface{}{}
+	mapsAsArrays := map[string]map[string]interface{}{}
+
+	for _, line := range os.Environ() {
+		kv := strings.SplitN(line, "=", 2)
+		k := kv[0]
+		v := kv[1]
+		if ! any(prefixFilter, func(x string) bool { return strings.HasPrefix(k, x) }) {
+			continue
+		}
+
+		current := parsed
+		parts := strings.Split(k, ".")
+
+		for i, part := range parts[:len(parts)-1] {
+			// for error messages and to store any maps parsed as arrays so we can delete their references later
+			fqkey := strings.Join(parts[0:i+1], ".")
+
+			// check for existence and then the proper type
+			if _, ok := current[part]; !ok {
+				current[part] = map[string]interface{}{}
+			}
+			if _, ok := current[part].(map[string]interface{}); !ok {
+				log.Fatalf("Wanted to parse a map out of %s, but it was already parsed as a non-map: %s", fqkey, current[part])
+			}
+
+			next := current[part].(map[string]interface{})
+			if strings.ContainsAny(part, "[") {
+				if err := parsePartAsArray(part, current, next); err != nil {
+					log.Fatalf("Failed to parse array out of %s: %s", fqkey, err)
+				}
+				mapsAsArrays[fqkey] = current
+			}
+			current = next
+		}
+
+		lastPart := parts[len(parts)-1]
+		if _, ok := current[lastPart]; ok {
+			log.Fatalf("Wanted to set %s=%s, but %s was already parsed as a map.", k, v, k)
+		}
+		if strings.ContainsAny(lastPart, "[") {
+			if err := parsePartAsArray(lastPart, current, sanitize(v)); err != nil {
+				log.Fatalf("Failed to parse array out of %s: %s", k, err)
+			}
+		} else {
+			current[lastPart] = sanitize(v)
+		}
+	}
+
+	// cleanup
+	for k, v := range mapsAsArrays {
+		parts := strings.Split(k, ".")
+		ogPart := parts[len(parts)-1]
+		delete(v, ogPart)
+	}
+
+	s, err := prettyJson(parsed)
+	if err != nil {
+		log.Fatalf("Failed to marshal")
+	}
+	log.Println(s)
+}
+
+// Given a key "p[n0][n1]...[nk]", an existing map m, and a value v,
+// this function creates entries in m such that m[p][n0][n1]...[nk]=v
+func parsePartAsArray(part string, m map[string]interface{}, v interface{}) error {
+	part = strings.ReplaceAll(part, "[", ".")
+	part = strings.ReplaceAll(part, "]", "")
+	subparts := strings.Split(part, ".")
+
+	key, indices := subparts[0], subparts[1:]
+	if _, ok := m[key]; !ok {
+		m[key] = &VariableArray{}
+	}
+	if _, ok := m[key].(*VariableArray); !ok {
+		return fmt.Errorf("Expected %s to be an array, but got: %s", key, m[key])
+	}
+	current := m[key].(*VariableArray)
+
+	nums := make([]int, len(indices))
+	for j, index := range indices {
+		i, err := strconv.Atoi(index)
+		if err != nil {
+			return fmt.Errorf("Found non-number %s in array index", index)
+		}
+		nums[j] = i
+	}
+
+	for j, i := range nums[:len(nums)-1] {
+		// similar to the map traversal, check for existence and the proper type until we get to the end
+		if val := current.Get(i); val == nil {
+			current.Set(i, &VariableArray{})
+		}
+		if _, ok := current.Get(i).(*VariableArray); !ok {
+			return fmt.Errorf("Wanted to parse an array out of %s[%s], but it was: %v", key, strings.Join(indices[0:j+1],"]["), current.Get(i))
+		}
+		current = current.Get(i).(*VariableArray)
+	}
+
+	lastIndex := nums[len(nums)-1]
+	if val := current.Get(lastIndex); val != nil {
+		// If m[p][n0][n1]...[nk] already exists, then there must exist a j>=k such that there exists
+		// a part "p[n0][n1]...[nj]" sharing common descendents with "p[n0][n1]...[nk]" in our env vars
+		// that was already parsed. Proof: If j<k, then we would have already failed in the loop above
+		// expecting m[p][n0][n1]...[nj] to be an array.
+		// 
+		// Now, if j>k, we can fail immediately since m[p][n0][n1]...[nk] cannot both be an array and
+		// a terminating value; e.g. a[0]=1 a[0][0]=2. More interestingly, if j=k, then we'd either have
+		// a duplicate env var (can't happen), or m[p][n0][n1]...[nk] was parsed as a map; e.g. a[0].a=1 a[0].b=2.
+		// So, fail on non-maps only:
+		if _, ok := val.(map[string]interface{}); !ok {
+			return fmt.Errorf("Wanted to set %s[%s]=%v; but %v is already there", key, strings.Join(indices,"]["), v, val)
+		}
+	}
+	current.Set(lastIndex, v)
+	return nil
+}
+
+
+func prettyJson(data interface{}) (string, error) {
+	json, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(json), nil
+}
+
+func any(xs []string, f func(string) bool) bool {
+	for _, x := range xs {
+		if f(x) {
+			return true
+		}
+	}
+	return false
+}
