63c1b6de38c062b97966cf17ee1a1597569ca7ab
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2019-06-17 01:10:20 -0400

    get rid of unnecessary wrapper struct for variable array

--- pkg/variable_array.go
+++ pkg/variable_array.go
@@ -7,43 +7,64 @@ import (
 
 // VariableArray represents an array that does not fail
 // whenever a get or set is performed on an out-of-bounds index.
-// Like JavaScript arrays <3 e.g.: a=[]; a[10]==undefined
-type VariableArray struct {
-	slice []interface{}
-}
+// Like JavaScript arrays <3.
+// It's represented as a pointer to a slice of `interface{}`.
+type VariableArray []interface{}
 
 // oh no so many new slices
 func (a *VariableArray) expandIfNecessary(i int) {
-	if i+1 > len(a.slice) {
+	if i+1 > len(*a) {
 		expanded := make([]interface{}, i+1)
-		copy(expanded, a.slice)
-		a.slice = expanded
+		copy(expanded, *a)
+		*a = expanded
 	}
 }
 
 func (a *VariableArray) Set(i int, val interface{}) {
 	a.expandIfNecessary(i)
-	a.slice[i] = val
+	(*a)[i] = val
 }
 
 func (a *VariableArray) Get(i int) interface{} {
-	if i+1 > len(a.slice) {
+	if i+1 > len(*a) {
 		return nil
 	}
-	return a.slice[i]
+	return (*a)[i]
 }
 
+// Iterator returns the underlying slice
 func (a *VariableArray) Iterator() []interface{} {
-	return a.slice
+	return *a
 }
 
+// IsWellDefined returns true if every element in the array
+// is of the same type.
+func (a *VariableArray) IsWellDefined() bool {
+	t := fmt.Sprintf("%T", a.Get(0))
+	for _, v := range *a {
+		if fmt.Sprintf("%T", v) != t {
+			return false
+		}
+	}
+	return true
+}
+
+func (a *VariableArray) GetType() string {
+	if a.IsWellDefined() {
+		return fmt.Sprintf("%T", a.Get(0))
+	}
+	return "mixed"
+}
+
+// Len returns the length of the underlying slice
 func (a *VariableArray) Len() int {
-	return len(a.slice)
+	return len(*a)
 }
 
+// LenNonEmpty returns the number of non-nil elements in the underlying slice
 func (a *VariableArray) LenNonEmpty() int {
 	count := 0
-	for _, v := range a.slice {
+	for _, v := range a.Iterator() {
 		if v != nil {
 			count++
 		}
@@ -53,7 +74,7 @@ func (a *VariableArray) LenNonEmpty() int {
 
 func (a *VariableArray) String() string {
 	s := []string{}
-	for _, v := range a.slice {
+	for _, v := range *a {
 		s = append(s, fmt.Sprintf("%v", v))
 	}
 	return "[" + strings.Join(s, " ") + "]"
